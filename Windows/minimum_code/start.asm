af35d540bef035d39a5061735dd4213cc561ec05
NotRobot <64023996+notrobot1@users.noreply.github.com>
2022-10-13 14:59:12 -0700

    Create start.asm

---
+++ Windows/minimum_code/start.asm
@@ -0,0 +1,25 @@
+
+;https://www.nasm.us/xdoc/2.11.08/html/nasmdoc6.html
+
+;EXTERN Импорт символов из других модулей
+extern _ExitProcess@4
+
+;Экспорт символов в другие модули
+global Start 
+
+
+;инициализированные данные
+section .data
+
+;неинициализированные данные
+section .bss
+
+;Code
+section .text
+    ; Главная функция
+	Start:
+	
+	; Завершение программы
+	exit:
+        push    NULL
+        call    _ExitProcess@4
