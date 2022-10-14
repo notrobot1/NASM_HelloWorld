c8dfc3405a91ea0b00db6a714dea62d2b888da0a
NotRobot <64023996+notrobot1@users.noreply.github.com>
2022-10-13 20:43:09 -0700

    Update start.asm

--- Windows/minimum_code/start.asm
+++ Windows/minimum_code/start.asm
@@ -12,13 +12,20 @@ global Start
 
 ;инициализированные данные
 section .data
- ;DB - Define Byte. 8 bits
+ ;;;DB - Define Byte. 8 bits
  ;DW - Define Word. Generally 2 bytes on a typical x86 32-bit system
  ;DD - Define double word. Generally 4 bytes on a typical x86 32-bit system
+ ;DB allocates in chunks of 1 byte.
+ ;DQ allocates in chunks of 8 bytes.
  ;array   dd 89, 10, 67, 1, 4, 27, 12, 34, 86, 3
 
 ;неинициализированные данные
 section .bss
+ ;RESB 1 allocates 1 byte.
+ ;RESW 1 allocates 2 bytes.
+ ;RESD 1 allocates 4 bytes.
+ ;RESQ 1 allocates 8 bytes.
+ ;varname resd 1
 
 ;Code
 section .text
